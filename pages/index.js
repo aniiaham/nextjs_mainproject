90a23ee77decd07a7f18d7331ec9ecd3ce648602
aniiaham <aniya_axm@icloud.com>
2023-02-17 18:00:36 -0500

    init

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/d/da/Allianz_arena_golden_hour_Richard_Bartz.jpg',
+        address: 'Somewhere 5, 12345 Some City',
+        description: 'This is a first meetup'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/d/da/Allianz_arena_golden_hour_Richard_Bartz.jpg',
+        address: 'Somewhere 5, 12345 Some City',
+        description: 'This is a second meetup'
+    },
+    {
+        id: 'm3',
+        title: 'A Third Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/d/da/Allianz_arena_golden_hour_Richard_Bartz.jpg',
+        address: 'Somewhere 5, 12345 Some City',
+        description: 'This is a third meetup'
+    }
+
+]
+
+function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+};
+
+export default HomePage;
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,12 @@
+//our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetUpPage() {
+
+    const addMeetupHandler = (enteredMeetupData) => {
+        console.log(enteredMeetupData)
+    }
+    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+};
+
+export default NewMeetUpPage;
