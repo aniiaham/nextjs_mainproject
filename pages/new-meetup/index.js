97cff21e9f5131c3a3a423e8996333dd02002475
aniiaham <aniya_axm@icloud.com>
2023-02-18 19:27:23 -0500

    added MeetupDetail

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,12 +1,15 @@
-//our-domain.com/new-meetup
-import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function NewMeetUpPage() {
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/da/Allianz_arena_golden_hour_Richard_Bartz.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
 
-    const addMeetupHandler = (enteredMeetupData) => {
-        console.log(enteredMeetupData)
-    }
-    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
-};
-
-export default NewMeetUpPage;
+export default MeetupDetails;
