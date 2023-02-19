9645b26bac63b923562f8c1e306ad6d0cb73060d
aniiaham <aniya_axm@icloud.com>
2023-02-18 20:27:02 -0500

    fixed mistake in index.js files

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/da/Allianz_arena_golden_hour_Richard_Bartz.jpg"
+      title="A First Meetup"
+      address="Somewhere 5, 12345 Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,15 +1,10 @@
-import { Fragment } from "react";
-import MeetupDetail from "../../components/meetups/MeetupDetail";
+//our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+function NewMeetupPage() {
+    function addMeetupHandler(enteredData) {
+        console.log(enteredData);
+    }
+    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+};
 
-function MeetupDetails() {
-  return (
-    <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/d/da/Allianz_arena_golden_hour_Richard_Bartz.jpg"
-      title="A First Meetup"
-      address="Somewhere 5, 12345 Some City"
-      description="The meetup description"
-    />
-  );
-}
-
-export default MeetupDetails;
+export default NewMeetupPage;
