dda7bb9d991ece20a7ee6c918c89503fecf3be11
aniiaham <aniya_axm@icloud.com>
2023-02-23 16:05:37 -0500

    api route

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,9 @@
 //our-domain.com/new-meetup
+import { useRouter } from 'next/router';
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const router = useRouter();
 
   async function addMeetupHandler(enteredData) {
     const response = await fetch("/api/new-meetup", {
@@ -14,6 +16,7 @@ function NewMeetupPage() {
     const data = await response.json();
     console.log(data);
     
+    router.push('/');
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
